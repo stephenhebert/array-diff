793fbfdbc4fe3b874031b02f1469138061cf006e
Stephen Hebert <stephenjosephhebert@gmail.com>
2024-06-12 19:07:18 -0500

    update examples

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import arrayDiff from "./arrayDiff"
 
 // standard usage
-const a = [{ id: 1, name: "Alice" }, { id: 2, name: "Bob" }]
-const b = [{ id: 2, name: "Bob" }, { id: 3, name: "Charlie" }]
-console.log(arrayDiff(a, b, { comparator: (a, b) => a.id === b.id }))
+const initialHumans = [{ id: 1, name: "Alice" }, { id: 2, name: "Bob" }, { id: 3, name: "Charlie" }]
+const finalHumans = [{ id: 2, name: "Blob" }, { id: 3, name: "Charlie" }, { id: 4, name: "David" }]
+console.log('diffHumans', arrayDiff(initialHumans, finalHumans, { comparator: (a, b) => a.id === b.id }))
 
 // simple array
-const c = [ 'Alice', 'Bob']
-const d = [ 'Bob', 'Charlie']
-console.log(arrayDiff(c, d, { comparator: (a, b) => a === b }))
+const initialFruits = [ 'Apple', 'Blueberry']
+const finalFruits = [ 'Blueberry', 'Cherry']
+console.log('diffFruits', arrayDiff(initialFruits, finalFruits, { comparator: (a, b) => a === b }))
+
+// exclude modified
+const initialCities = [{ id: 1, name: "Atlanta" }, { id: 2, name: "Boston" }, { id: 3, name: "Chicago" }]
+const finalCities = [{ id: 2, name: "Boston" }, { id: 3, name: "Chicargo" }, { id: 4, name: "Denver" }]
+console.log('diffCities', arrayDiff(initialCities, finalCities, { comparator: (a, b) => a.id === b.id, includeModified: false}))
